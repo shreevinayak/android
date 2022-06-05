e5040e262cffffd2daa60f7a6bac306ef2270566
shreevinayak <106444053+shreevinayak@users.noreply.github.com>
2022-06-05 19:19:51 +0530

    Update MainActivity.java

--- pr21/MainActivity.java
+++ pr21/MainActivity.java
@@ -1,27 +1,31 @@
 package mad.practicals.pr21;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
+
+    AirplaneModeChangeReceiver airplaneModeChangeReceiver = new AirplaneModeChangeReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button b1 = findViewById(R.id.broadcast);
-        b1.setOnClickListener(v ->{
-            broadCastIntent();
-        });
     }
 
-    public void broadCastIntent(){
-        Intent intent = new Intent();
-        intent.setAction("mad.practicals.pr21.myReciever");
-        sendBroadcast(intent);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(airplaneModeChangeReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(airplaneModeChangeReceiver);
     }
 }
